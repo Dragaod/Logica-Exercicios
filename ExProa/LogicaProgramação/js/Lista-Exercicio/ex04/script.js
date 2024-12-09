b001f4dfdbae72c7de25240355a148fd3710e020
Ryan Honorato <ryanhonoratobsouza@Gmail.com>
2024-12-09 20:24:13 -0300

    Exercicio-Logica

---
+++ ExProa/LogicaProgramação/js/Lista-Exercicio/ex04/script.js
@@ -0,0 +1,19 @@
+let A = parseFloat(prompt('Digite o valor de A:')) 
+let B = parseFloat(prompt('Digite o valor de B:')) 
+let C = parseFloat(prompt('Digite o valor de C:')) 
+
+if (A > B && C < B){
+    document.write("O número maior é " + A + " e depois " + B + " e " + C)
+}else if (B > C && C > A){
+    document.write("O número maior é " + B + " e depois " + C + " e " + A)
+}else if(C > A && A > B){
+    document.write("O número maior é " + C + " e depois " + A + " e " + B)
+}else if(A > C && C > B){
+    document.write("O número maior é " + A + " e depois " + C + " e " + B)
+}else if(A < B && A > C){
+    document.write("O número maior é " + B + " e depois " + A + " e " + C)
+}else if(C > A && B > A){
+    document.write("O número maior é " + C + " e depois " + B + " e " + A)
+}else{
+    document.write("valor invalido")
+}
